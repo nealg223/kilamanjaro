1c92d1790f8b1dabcd92bfa9f9662fec6ec70628
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-02 00:17:01 -0500

    added Devise Gem, setup the routes proper, learningz iz hapnin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Kilamanjaro::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
+  devise_for :students
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20110701031232_devise_create_students.rb
@@ -0,0 +1,29 @@
+class DeviseCreateStudents < ActiveRecord::Migration
+  def self.up
+    create_table(:students) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+
+      # t.encryptable
+      # t.confirmable
+      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      # t.token_authenticatable
+
+      # t.string  :name, :limit => 100, :null => false
+
+      t.timestamps
+    end
+
+    add_index :students, :email,                :unique => true
+    add_index :students, :reset_password_token, :unique => true
+    # add_index :students, :confirmation_token,   :unique => true
+    # add_index :students, :unlock_token,         :unique => true
+    # add_index :students, :authentication_token, :unique => true
+  end
+
+  def self.down
+    drop_table :students
+  end
+end
